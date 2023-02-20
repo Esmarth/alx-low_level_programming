e5ee7393f76ecd36b88b0da9637741b4613464a3
Esmarth <ozuruemmanuel1@gmail.com>
2023-02-20 00:34:52 -0800

    bike

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'z';
+	char last = 'a';
+
+	while (letter >= last)
+	{
+		putchar(letter);
+		letter--;
+	}
+
+	putchar('\n');
+	return (0);
+}
+

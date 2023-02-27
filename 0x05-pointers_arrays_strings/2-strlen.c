da16112b645cde81d6056089d596af993101e208
Esmarth <ozuruemmanuel1@gmail.com>
2023-02-27 15:52:58 -0800

    sleep

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ *_strlen - function for calc string lenght
+ *@s: string to be calculated
+ *Return: count
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (; *s++;)
+		i++;
+	return (i);
+}
